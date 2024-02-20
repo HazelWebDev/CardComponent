2f3b5e667807ce1610f346c85b5869d165958e65
Kristen Roggero <kroggero@Kristen-Roggeros-MacBook-Air.local>
2024-02-20 12:32:26 -0600

    Second Commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,68 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        {/* Should contain one Skill component
+        for each web dev skill that you have,
+        customized with props */}
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return(
+    <img className="avatar" src="Purple Watercolor Desktop Wallpaper.png"></img>
+  )
+}
+
+function Intro() {
+  return(
+    <div>
+      <h2 className="h1">Hi, I'm Kristen</h2>
+      <p className="body">I'm a small town web developer who is fascinated by 
+      the creativity of web development!</p>
+    </div>
+  )
+}
+
+function SkillList() {
+  return(
+    <div className="skill-list">
+      <Skill skill="HTML & CSS" color="#D4BEBE" />
+      <Skill skill="JavaScript" color="#E1DEE9"/>
+      <Skill skill="Web Development" color="#D5CFE1"/>
+      <Skill skill="React.JS" color="#B6A6CA"/>
+      <Skill skill="TailwindCSS" color="#A09ABC"/>
+
+    {/* or do it this way without props, but you lose cool points
+    <h6 className="skill">Web Development</h6>
+    <h6 className="skill">TailwindCSS</h6>
+    <h6 className="skill">React.JS</h6> */}
+    </div>
+  )
+}
+
+function Skill(props) {
+  return(
+    <div className="skill" style={{backgroundColor: props.color}}>
+      <span>{props.skill}</span>
+    </div>
+  )
+}
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
